d1f2e61c042d929f3910e3f16418f39489815389
Daniel Temkin <d@danieltemkin.com>
2023-07-22 15:48:49 -0400

    Partial refactor of parser

--- velato_token.js
+++ velato_token.js
@@ -3,6 +3,10 @@ window.velato = window.velato || {}
 
 /*
  * A velato.token is the basic parsing unit of the language
+ *
+ * From a series of these tokens, one should be able to determine:
+ * * the entire program as a series of notes
+ * * the entire program as js commands
  */
 velato.token = function() {
 
@@ -10,29 +14,42 @@ velato.token = function() {
 
     this.sequence = []; // used for numerics or chars
 
-    this.print = undefined;
+    this._print = undefined;
+
+    this.lexnode = undefined; // the node in lexicon.json that this token is identified as
 
     this.add_note = function(note) {
         this.notes.push(note);
     }
 
+    this.setlex = function(node) {
+        this.lexnode = structuredClone(node);
+        this.type = node.type;
+        this.name = node.name;
+        this.desc = node.desc;
+        this._print = node.print;
+        this.childCmds = node.childCmds;
+
+        this.children = structuredClone(node.children);
+    }
+
     // called to produce js text from notes
     this.evaluate = function() {
-        if (!this.print)
+        if (!this._print)
             return "";
 
-        return this.print
+        return this._print
             .replace("{varname}", this.sequence[0]) // this should already be set to a variable name in this case
             .replace("{seq_int}", this.sequence.join())
             .replace("{seq_char}", String.fromCharCode(this.sequence.join()));
     }
 
-    this.clone = function() {
-        clone = new velato.token();
-        clone.notes = this.notes.slice(0); // copy array
-        clone.sequence = this.sequence;
-        clone.print = this.print;
-        return clone;
-    }
+    // this.clone = function() {
+    //     clone = new velato.token();
+    //     clone.notes = this.notes.slice(0); // copy array
+    //     clone.sequence = this.sequence.slice(0);
+    //     clone._print = this._print;
+    //     return clone;
+    // }
 }
 
