887ce4f6832b08ab7650226106b03d5c915dcd82
Daniel Temkin <d@danieltemkin.com>
2025-07-09 10:03:54 -0400

    Working tests for Root Change and Reset

--- tests/programbuilder.test.js
+++ tests/programbuilder.test.js
@@ -9,10 +9,56 @@ velato.programbuilder = require('../velato_programbuilder').programbuilder;
 velato.ObjPb = require('../velato_programbuilder').ObjPb;
 velato.web_display = require('./velato_tester_display');
 
-beforeEach(() => {
-    jest.clearAllMocks();
-    jest.restoreAllMocks();
-});
+// beforeEach(() => {
+//     jest.clearAllMocks();
+// });
+
+const get_note = (name, octave) => {
+    // Mocks how intervals are assigned in velato_audio_interface
+    // The interval numbers are used to calculate distance, and so must be populated correctly.
+    switch (name) {
+        case "B":
+            if (octave == 5)
+                return new velato.note("B", octave, null, 71, null);
+            else if (octave == 6)
+                return new velato.note("B", octave, null, 83, null);
+        case "C":
+            if (octave == 6)
+                return new velato.note("C", octave, null, 72, null);
+        case "C♯ / D♭":
+            if (octave == 6)
+                return new velato.note("C♯ / D♭", octave, null, 73, null);
+        case "D":
+            if (octave == 6)
+                return new velato.note("D", octave, null, 74, null);
+        case "D♯ / E♭":
+            if (octave == 6)
+                return new velato.note("D♯ / E♭", octave, null, 75, null);
+        case "E":
+            if (octave == 6)
+                return new velato.note("E", octave, null, 76, null);
+        case "F":
+            if (octave == 6)
+                return new velato.note("F", octave, null, 77, null);
+        case "F♯ / G♭":
+            if (octave == 6)
+                return new velato.note("F♯ / G♭", octave, null, 78, null);
+        case "G":
+            if (octave == 6)
+                return new velato.note("G", octave, null, 79, null);
+        case "G♯ / A♭":
+            if (octave == 6)
+                return new velato.note("G♯ / A♭", octave, null, 80, null);
+        case "A":
+            if (octave == 6)
+                return new velato.note("A", octave, null, 81, null);
+        case "A♯ / B♭":
+            if (octave == 6)
+                return new velato.note("A♯ / B♭", octave, null, 82, null);
+        default:
+            throw new Error(`Unknown note: ${name} ${octave}`);
+    }
+};
 
 test('programbuilder starts up', () => {
     root_note = velato.programbuilder.root_note;
@@ -27,7 +73,7 @@ test('no root note on startup', () => {
 test('first note sets root note: C', () => {
     // velato.programbuilder.reset_program(); // must reset first, as these functions overlap    
 
-    velato.programbuilder.add_tone(new velato.note("C",6,null,72,null));
+    velato.programbuilder.add_tone(get_note("C",6));
     root_note = velato.programbuilder.root_note;
     expect(root_note).toBeDefined();
     expect(root_note.name).toBe("C");
@@ -38,7 +84,7 @@ test('first note sets root note: B', () => {
     pb = new velato.ObjPb();
     pb.BEG_PROGRAM = '';
 
-    pb.add_tone(new velato.note("B",6,null,71,null));
+    pb.add_tone(get_note("B",5));
     root_note = pb.root_note;
     expect(root_note).toBeDefined();
     expect(root_note.name).toBe("B");
@@ -53,16 +99,16 @@ test('change root note', () => {
     pb = new velato.ObjPb();
     pb.BEG_PROGRAM = ''; 
 
-    pb.add_tone(new velato.note("C",6,null,72,null));
-    pb.add_tone(new velato.note("D",6,null,74,null));
-    pb.add_tone(new velato.note("E",6,null,76,null));
+    pb.add_tone(get_note("C",6));
+    pb.add_tone(get_note("D",6));
+    pb.add_tone(get_note("E",6));
 
     root_note = pb.root_note;
     expect(root_note).toBeDefined();
     expect(root_note.name).toBe("E");
 });
 
-test('change root note 2', () => {
+test('change root note to accidental', () => {
 
     velato.web_display.write_full_callback = (js_program) => {
         output = js_program;
@@ -71,11 +117,29 @@ test('change root note 2', () => {
     pb = new velato.ObjPb();
     pb.BEG_PROGRAM = ''; 
 
-    pb.add_tone(new velato.note("B",6,null,71,null));
-    pb.add_tone(new velato.note("C",6,null,72,null));
-    pb.add_tone(new velato.note("B",6,null,71,null));
+    pb.add_tone(get_note("B",5));
+    pb.add_tone(get_note("C",6));
+    pb.add_tone(get_note("C♯ / D♭",6));
 
     root_note = pb.root_note;
     expect(root_note).toBeDefined();
-    expect(root_note.name).toBe("B");
+    expect(root_note.name).toBe("C♯ / D♭");
+});
+
+test('reset: first following note sets root', () => {
+
+    velato.web_display.write_full_callback = (js_program) => {
+        output = js_program;
+    };
+
+    pb = new velato.ObjPb();
+    pb.BEG_PROGRAM = ''; 
+
+    pb.add_tone(get_note("B",5));
+    pb.reset_program();
+    pb.add_tone(get_note("C♯ / D♭",6));
+
+    root_note = pb.root_note;
+    expect(root_note).toBeDefined();
+    expect(root_note.name).toBe("C♯ / D♭");
 });
